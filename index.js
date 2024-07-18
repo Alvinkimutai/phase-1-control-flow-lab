5f82f65ae9b9a58c8aa93cf7466ce06aeea8f184
Alvinkimutai <alvinchepkiyeng@gmail.com>
2024-07-18 10:37:44 +0300

    finishes control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+ let greeting
+    if (feet<400){
+     greeting = "This one is on me!"
+     return greeting;
+    }
+    else if (feet>=400 && feet <=2000){
+      greeting= "That will be twenty bucks."
+      return greeting;
+    }
+    else if (feet>=2000 && feet <=2500){
+      greeting= "I will gladly take your thirty bucks."
+      return greeting;
+    }
+    else{
+      greeting= 'No can do.'
+      return greeting;
+    }
 }
+console.log(scuberGreetingForFeet(2501));
+let city = 'Pittsburgh';
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC'? 'Ok, sounds good.': 'No go.';
 }
+console.log(ternaryCheckCity(city));
+
+
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  let charm;
+  switch (tip){
+    case 'generous':
+      charm = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      charm = 'Thank you.';
+      break;
+    default:
+      charm = 'Bye.'
+      break;
+
+  }
+  return charm;
+}
+console.log(switchOnCharmFromTip('generous'));
+
+// Bonus function while I was practicing
+
+function addFive (someNumber) {
+  let result;
+  if (someNumber>0) {
+    result = someNumber+5;
+  }
+  return result
+}
+console.log(addFive(5))
